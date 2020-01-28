1464bbd5e99fee69dc3efb5f956f98ef048b1e30
Ammar <ammar.n.sammour@gmail.com>
2020-01-28 16:16:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+let game = document.getElementById("game");
+let width = game.width;
+let widthNo = parseInt(width, 10);
+
+function moveDodgerLeft() {
+    let leftNo = dodger.style.left.replace("px", "");
+    let leftNumber = parseInt(leftNo, 10);
+    if (leftNumber > 0) {
+        dodger.style.left = leftNumber - 1 + "px";
+    }
+}
+
+function moveDodgerRight() {
+    let leftRightNo = dodger.style.left.replace("px", "");
+    let leftInt = parseInt(leftRightNo, 10);
+    if (leftInt < 360) dodger.style.left = leftInt + 1 + "px";
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        goToLeft();
+    }
+
+    if (e.key === "ArrowRight") {
+        goToRight();
+    }
+});
