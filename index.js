bfb041bb8a79da9e56d24a2cc74f5ab9512acc51
allan <allansaleh95@gmail.com>
2020-03-29 12:07:38 +0300

    added another function

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // Your code here
 let dodger=document.getElementById('dodger');
 
-function rightArrowKey(){
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers);
+
+    if(left>0){
+        dodger.style.left = `${left-1}px`;
+    }
+}
+
+function moveDodgerRight(){
     let rightNumbers = dodger.style.left.replace("px","");
     let right = parseInt(rightNumbers);
 
@@ -12,6 +21,10 @@ function rightArrowKey(){
 
 document.addEventListener("keydown",function(e){
     if(e.key === "ArrowRight"){
-        rightArrowKey();
+        moveDodgerRight();
+    }
+
+    if(e.key === "ArrowLeft"){
+        moveDodgerLeft();
     }
 });
