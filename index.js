9d526b744b50c786a941091c24c517158166b8a8
allan <allansaleh95@gmail.com>
2020-03-29 12:01:16 +0300

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+let dodger=document.getElementById('dodger');
+
+function rightArrowKey(){
+    let rightNumbers = dodger.style.left.replace("px","");
+    let right = parseInt(rightNumbers);
+
+    if(right>0){
+        dodger.style.left = `${right+1}px`;
+    }
+}
+
+document.addEventListener("keydown",function(e){
+    if(e.key === "ArrowRight"){
+        rightArrowKey();
+    }
+});
