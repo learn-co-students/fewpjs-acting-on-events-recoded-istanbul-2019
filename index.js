25da55dba0e8f931f661b0e7889db6b03be42f4f
samah_mustafa <i.samaaah@gmail.com>
2020-04-19 18:53:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+
+let dodger = document.querySelector('#dodger')
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+      dodger.style.left = `${left - 1}px`;
+
+  }
+
+
+  document.addEventListener("keydown", function(e) {
+    console.log(e.key);
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if(e.key ==="ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+  /////
+  
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+      dodger.style.left = `${left + 1}px`;
+   
+  }
+
+
